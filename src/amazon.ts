cc21d5e29d050eb0e9b52a620da79e91fb077c33
An Hoang <an.hoang@tu-braunschweig.de>
2024-09-29 17:28:37 +0200

    Use products.ts file

--- src/amazon.ts
+++ src/amazon.ts
@@ -1,42 +1,5 @@
 // 57
-
-type Product = {
-  image: string,
-  name: string,
-  rating: {
-    stars: number,
-    count: number
-  },
-  priceCents: number,
-}
-
-const products: Product[] = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+import { products } from './data/products.ts';
 
 let productsHTML: string = '';
 
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: string,
+  image: string,
+  name: string,
+  rating: {
+    stars: number,
+    count: number
+  },
+  priceCents: number,
+  keywords: string[],
+  type?: string,
+  sizeChartLink?: string
+}
