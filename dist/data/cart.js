d5f1edda01c3bcea2c0e0cdc2e1a6be1858c4659
An Hoang <an.hoang@tu-braunschweig.de>
2024-09-30 19:07:41 +0200

    Generate HTML for order summary

--- dist/data/cart.js
+++ dist/data/cart.js
@@ -1,5 +1,12 @@
 import { getElement } from '../dom-utils.js';
-const cart = [];
+const cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
 function handleAddToCart(productId) {
     const quantitySelector = getElement(`.js-quantity-selector-${productId}`);
     const quantity = Number(quantitySelector.value);
--- src/data/cart.ts
+++ src/data/cart.ts
@@ -1,7 +1,14 @@
 import { getElement } from '../dom-utils.js';
 import { CartProduct } from '../types.js';
 
-const cart: CartProduct[] = [];
+const cart: CartProduct[] = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 function handleAddToCart(productId: string): void {
   const quantitySelector = getElement<HTMLSelectElement>(
